7b0c967c7f3741d4733b31dab4aa25bb5e3ee0b5
Ángela Amanda Sánchez Barbosa <61953402+angelexi76@users.noreply.github.com>
2022-08-11 12:30:04 -0500

    Add files via upload

---
+++ Taller1.java
@@ -0,0 +1,66 @@
+
+package Manager1;
+//importacion de las clases
+import ClasesSofka.BankAccount;
+import ClasesSofka.Person;
+
+
+
+import java.util.ArrayList;
+
+        
+        
+public class Taller1 {
+//metodo ejecutable  main  
+  public static void main(String[] args) {
+
+//crea la instancia de la clase Cuenta Bancaria   
+BankAccount Bank1 = new BankAccount();
+
+//llamado al metodo estado de cuenta de la clase Cuenta Bancaria
+    Bank1.accountStatus();
+    System.out.println("");
+    
+ 
+       
+//instancia de arratList
+
+    ArrayList<String> color = new ArrayList<>();
+// agrego elementos alarrayList
+      color.add("purple");
+      color.add("Red");
+      color.add("yellow");
+
+      
+//recorro el Array List con un for
+//uso de size porque es un arrayList y no length como en un array
+    for (int i = 0; i < color.size(); i++) {
+        System.out.println("Color:\n");
+        System.out.println("-->"+color.get(i));   
+      }
+    
+    
+//instancia objeto persona
+    Person Person1 = new Person();
+       Person1.setName("Ángela");
+       Person1.setLastName1("Sánchez");
+       Person1.setLastName2("Barbosa");
+       Person1.setHeight((float) 1.60);
+
+    System.out.println("Person:"+ Person1.getName()+Person1.getLastName1()+Person1.getLastName2());
+      
+}
+
+   
+
+    
+  
+
+   
+   
+    
+    
+}
+
+
+    
---
+++ Tournament.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Manager2;
+ //importacion de las clases
+import MisClases.Coach;
+import MisClases.Team;
+import MisClases.Sporty;
+/**
+ *
+ * @author angelaSanchez
+ */
+public class Tournament {
+    
+//metodo ejecutable   
+    public static void main(String[] args) {
+        
+//  instancio el objeto equipo
+      
+    Team Team1 = new Team();
+    Team1.setId(01);
+    Team1.setName("Dodgers");
+    Team1.setSport("baseball");
+    System.out.println("el Equipo:" +""+ Team1.getName()+" "+"jugara:"+ Team1.getSport());
+    
+    
+    
+//instanciacion object Coach
+    Coach Coach1 =new Coach();
+    Coach1.setName("Clayton McCullough");
+    Coach1.setIdentityCard("12345");
+    System.out.println(" Su Coach es:"+Coach1.getName());
+    
+    
+    
+//instancia object deportista
+    Sporty Sporty1 =  new Sporty();
+    Sporty1.setId(01);
+    Sporty1.setName("Spencer");
+    Sporty1.setSurname("Torkelson");
+    Sporty1.setIdentityCard("741741");
+    
+     System.out.println("El Nombre del Jugador es:"+Sporty1.getName()+" "+Sporty1.getSurname());
+    }
+    
+}
+
+ 
